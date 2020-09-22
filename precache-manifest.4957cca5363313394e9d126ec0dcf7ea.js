f0de3ad2eb3f91db40d2be006ca64f8c92830953
Tatyana <hanyatan@gmail.com>
2020-09-22 14:19:18 -0400

    Updates

---
+++ precache-manifest.4957cca5363313394e9d126ec0dcf7ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f698b4e243101975fa1df565f89169c7",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "0b1e36f6a890fbda5519",
+    "url": "/React-Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d66582f0a7c794b724e0",
+    "url": "/React-Employee-Directory/static/css/main.07e36eb3.chunk.css"
+  },
+  {
+    "revision": "0b1e36f6a890fbda5519",
+    "url": "/React-Employee-Directory/static/js/2.42d35e3b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Employee-Directory/static/js/2.42d35e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66582f0a7c794b724e0",
+    "url": "/React-Employee-Directory/static/js/main.5bf53a19.chunk.js"
+  },
+  {
+    "revision": "94090917907741daeca2",
+    "url": "/React-Employee-Directory/static/js/runtime-main.45997ab5.js"
+  }
+]);
