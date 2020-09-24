ad81242b5a62c399454733f84c8395f03373fc9b
Tatyana <hanyatan@gmail.com>
2020-09-24 17:14:07 -0400

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,10 @@ function Header() {
           // backgroundImage: "linear-gradient(120deg,pink,blue)", 
           width: "120%"
   
-          }}>Employee Directory</h1>
+          }}>Employee Directory
+          
+          </h1>
+          <span>You can sort employees by First Name, Last Name, Age, Phone number, Email and Country from A to Z and from 0 to 9</span>
       </header>
   );
 }
